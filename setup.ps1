20aadcb9ea76c16259af4440342d02f5839b3dc5
dustyw0lf <94754217+dustyw0lf@users.noreply.github.com>
2025-04-17 23:24:17 +0300

    feat: remove hardcoded username

--- setup.ps1
+++ setup.ps1
@@ -5,8 +5,8 @@ https://github.com/epi052/osed-scripts/blob/main/install-mona.ps1
 #>
 
 $ShareDir = "\\tsclient\_home_kali_share\windbg\"
-$InstallDir = "C:\Users\Offsec\Desktop\windbg"
-$MonaLogsDir = "C:\Users\Offsec\Desktop\monalogs"
+$InstallDir = "$HOME\Desktop\windbg"
+$MonaLogsDir = "$HOME\Desktop\monalogs"
 
 Write-Output "[+] creating installation directory: $InstallDir"
 New-Item -Path $InstallDir -Type Directory | Out-Null
@@ -23,12 +23,12 @@ Start-Process -FilePath "$InstallDir\vcredist_x86.exe" -Wait
 # Move-Item "C:\Program Files\Windows Kits\10\Debuggers\x86\winext\pykd.pyd" "C:\Program Files\Windows Kits\10\Debuggers\x86\winext\pykd.pyd.bak"
 # Move-Item "C:\Program Files\Windows Kits\10\Debuggers\x86\winext\pykd.dll" "C:\Program Files\Windows Kits\10\Debuggers\x86\winext\pykd.dll.bak"
 
-# install python2.7
+# install Python 2.7
 Write-Output "[+] installing Python 2.7"
 Copy-Item "$ShareDir\python-2.7.18.msi" $InstallDir
 Start-Process -FilePath msiexec.exe -ArgumentList "/i $InstallDir\python-2.7.18.msi /qn" -Wait
 
-# register Python2.7 binaries in path before Python3
+# register Python 2.7 binaries in path before Python 3
 Write-Output "[+] adding python2.7 to the PATH"
 $p = [System.Environment]::GetEnvironmentVariable('Path', [System.EnvironmentVariableTarget]::User)
 [System.Environment]::SetEnvironmentVariable('Path', "C:\Python27\;C:\Python27\Scripts;" + $p, [System.EnvironmentVariableTarget]::User)
@@ -43,4 +43,4 @@ Copy-Item "$ShareDir\find-bad-chars.py" "C:\Program Files\Windows Kits\10\Debugg
 Write-Output "[+] registering runtime debug dll"
 regsvr32 "C:\Program Files\Common Files\Microsoft Shared\VC\msdia90.dll"
 
-Copy-Item \\tsclient\_home_kali_share\windbg\dark-green-x64.wew C:\Users\Offsec\Desktop\windbg\dark-green-x64.wew
+Copy-Item \\tsclient\_home_kali_share\windbg\dark-green-x64.wew $HOME\Desktop\windbg\dark-green-x64.wew
